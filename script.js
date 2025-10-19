fce2114a06883854a6adace4eab3ae70ded842bb
ThiaguinhoxD <thiaguinhoescolhido26@gmail.com>
2025-10-19 06:13:03 -0300

    adicionando botoes e dark theme

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.getElementById("theme-toogle");
+const body = document.body
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-theme");
+    body.classList.toggle("light-theme");
+
+    if (body.classList.contains("dark-theme")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
+});
